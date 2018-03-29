2bccef3866947d1c1153c00cebe6c0b504ba61aa
d10xa <d10xa@mail.ru>
2018-03-29 21:10:03 +0300

    add jadd.rb

---
+++ jadd.rb
@@ -0,0 +1,11 @@
+class Jadd < Formula
+  desc ""
+  homepage ""
+  url "https://bintray.com/d10xa/bin/download_file?file_path=jadd-0.1.0.zip"
+  sha256 "93b95cd1dfdaa141a5a25f52c44bec4b76e353b66badc5011dcd89085f81fe77"
+
+  def install
+      bin.install "bin/jadd"
+  end
+
+end

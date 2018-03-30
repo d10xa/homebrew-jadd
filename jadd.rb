88aaa26088bfe950befc0db6f2afbe0b8733e864
Andrey Stolyarov <d10xa@mail.ru>
2018-03-30 15:27:10 +0300

    Update jadd.rb

--- jadd.rb
+++ jadd.rb
@@ -5,8 +5,8 @@ class Jadd < Formula
   sha256 "93b95cd1dfdaa141a5a25f52c44bec4b76e353b66badc5011dcd89085f81fe77"
 
   def install
+      lib.install Dir["lib/*"]
       bin.install "bin/jadd"
-      lib.install "lib"
   end
 
 end

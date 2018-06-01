956139f900f1131e0cc43bba88e0ffa9fb5bc948
Andrey Stolyarov <d10xa@mail.ru>
2018-06-01 20:53:53 +0300

    Update jadd.rb

--- jadd.rb
+++ jadd.rb
@@ -1,8 +1,8 @@
 class Jadd < Formula
   desc "Command-line tool for adding dependencies to gradle/maven/sbt build files"
   homepage "https://github.com/d10xa/jadd"
-  url "https://bintray.com/d10xa/bin/download_file?file_path=jadd-0.1.9.zip"
-  sha256 "439c136bc90a3fdf52af12aa38ae51f8356654710d1bc1876e25eed6e2375d83"
+  url "https://bintray.com/d10xa/bin/download_file?file_path=jadd-0.1.10.zip"
+  sha256 "37e138f432c60f4cccca3087d203b7fc1f703bf0c7c5001bea82fae17e7520e7"
 
   def install
       lib.install Dir["lib/*"]
